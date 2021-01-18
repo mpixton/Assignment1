81b2c39a9cfae6507cd567b1b2b7368b736aab63
mpixton <71086131+mpixton@users.noreply.github.com>
2021-01-18 10:58:12 -0700

    Commit before turning in assignment

---
+++ wwwroot/js/gradecalculator.js
@@ -0,0 +1,14 @@
+﻿var submit = document.getElementById("submit-button")
+
+submit.addEventListener("click", function () {
+    alert('Your grade would be '
+        + ((
+            ((parseInt(document.getElementById('assignments').value) / 1000) * .5)
+            + ((parseInt(document.getElementById('group-projects').value) / 200) * .1)
+            + ((parseInt(document.getElementById('quizzes').value) / 200) * .1)
+            + ((parseInt(document.getElementById('exams').value) / 400) * .2)
+            + ((parseInt(document.getElementById('intex').value) / 200) * .1)
+        ) * 100)
+        + '%.'
+    );
+});
