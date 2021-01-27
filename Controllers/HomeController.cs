586953559dc2e211db011c9abc531f9ad9c26c7d
mpixton <71086131+mpixton@users.noreply.github.com>
2021-01-27 10:27:38 -0700

    DOC: update documentation
    Added comments to HomeController and gradecalculator.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,20 +9,26 @@ namespace MicahsFirstASPNETWebApp.Controllers
 {
     public class HomeController : Controller
     {
+        // GET /
         public IActionResult Index()
         { 
             return View();
         }
 
+        // GET /HOME/GradeCalc
         [HttpGet("GradeCalc")]
         public IActionResult GradeCalc()
         {
             return View();
         }
 
+        // POST /Home/GradeCalc
         [HttpPost("GradeCalc")]
         public IActionResult GradeCalc (GradeCalcModel model)
         {
+            // check if the model is valid
+            // if valid, return a blank view
+            // if invalid, return a view with error messsages
             if (ModelState.IsValid)
             {
                 return View();
--- wwwroot/js/gradecalculator.js
+++ wwwroot/js/gradecalculator.js
@@ -1,15 +1,35 @@
 ﻿//listens for a submission in the form 
-//alerts the user what their percentage grade would be in the class
+//alerts the user what their grade would be in the class
 $("#submit-button").click(function () {
-    alert('Your grade would be '
-        + ((
-            (parseFloat($('#assignments').val()) * .5)
-            + (parseFloat($('#group-projects').val()) * .1)
-            + (parseFloat($('#quizzes').val()) * .1)
-            + (parseFloat($('#exams').val()) * .2)
-            + (parseFloat($('#intex').val()) * .1)
-        ) * 100)
-        + '%.'
+    //determines the percentage grade
+    let dGrade =
+        (parseFloat($('#assignments').val()) * .5)
+        + (parseFloat($('#group-projects').val()) * .1)
+        + (parseFloat($('#quizzes').val()) * .1)
+        + (parseFloat($('#exams').val()) * .2)
+        + (parseFloat($('#intex').val()) * .1)
+    
+    //determines the letter grade
+    let sLetterGrade = ''
+
+    if (dGrade > 60) { sLetterGrade = 'D-' }
+    if (dGrade > 63) { sLetterGrade = 'D' }
+    if (dGrade > 67) { sLetterGrade = 'D+' }
+    if (dGrade > 70) { sLetterGrade = 'C-' }
+    if (dGrade > 73) { sLetterGrade = 'C' }
+    if (dGrade > 77) { sLetterGrade = 'C+' }
+    if (dGrade > 80) { sLetterGrade = 'B-' }
+    if (dGrade > 83) { sLetterGrade = 'B' }
+    if (dGrade > 87) { sLetterGrade = 'B+' }
+    if (dGrade > 90) { sLetterGrade = 'A-' }
+    if (dGrade > 93) { sLetterGrade = 'A' }
+    else { sLetterGrade ='E' }
+
+    //alerts the user of their grade
+    alert(
+        'Your grade would be: '
+        + sLetterGrade
+        + ' (' + dGrade + '%)'
     );
 });
 
